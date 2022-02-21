635ac3df86d009bc2d50a90588ea472df2f50329
EC2 Default User <ec2-user@ip-172-31-47-62.ec2.internal>
2022-02-21 12:36:56 +0000

    edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,34 +2,35 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to '/books'
-   
+
   end
-  
+
 
   def index
-    @books = Book.all 
+    @books = Book.all
   end
 
 
   def show
         @book =Book.find(params[:id])
-         
+
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
   def book_params
      params.require(:book).permit(:title, :body,)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'book/:id' => 'books#show'
   # get 'edit' => 'books#edit'
 
-  
+
   # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
