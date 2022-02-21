42001697b063500e2ab8a1944fff71903891c57c
EC2 Default User <ec2-user@ip-172-31-47-62.ec2.internal>
2022-02-21 10:15:04 +0000

    課題途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
   get 'top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
+  # post 'books'=>'books#create'
+  # get 'books' => 'books#index'
+
+  # get 'book/:id' => 'books#show'
+  # get 'edit' => 'books#edit'
+
+  
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
